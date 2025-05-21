04604d0f0e1e08689d76a8a81b7bb5de7fe207e2
Ashan-Wijesinghe-997 <ashanbuddhika997@gmail.com>
2025-05-21 08:27:28 +0530

    cahnge Brand section

--- frontend/components/Brandlogo.jsx
+++ frontend/components/Brandlogo.jsx
@@ -2,9 +2,9 @@ export default function BrandLogos() {
   const brands = ['SONY', 'Spotify', 'Canon', 'Walmart', 'Shopify'];
 
   return (
-    <section className="py-10 bg-black rounded-3xl mx-4">
-      <div className="max-w-7xl mx-auto px-8">
-        <div className="flex flex-wrap justify-center md:justify-between items-center gap-8 md:gap-4">
+    <section className="py-10 bg-black rounded-3xl mx-10 md:mx-20 lg:mx-32">
+      <div className="max-w-7xl mx-auto px-4 md:px-8">
+        <div className="flex flex-wrap justify-center md:justify-between items-center gap-8 md:gap-4 ml-5 mr-5">
           {brands.map((brand, index) => (
             <div
               key={index}
